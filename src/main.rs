44b35a798cee50f9861d87888573bdc070a8658c
Bin Wang <wkk@users.noreply.github.com>
2021-12-06 17:36:46 -0500

    Implement day 01

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+use clap::Parser;
+
+mod solution;
+mod y21;
+
+#[derive(Parser)]
+#[clap(version = "0.1.0", author = "Bin Wang <wkk@users.noreply.github.com>")]
+struct Opts {
+    day: u32,
+    part: u32,
+    input_file: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let opts: Opts = Opts::parse();
+    let solution_by_day: Box<dyn solution::Solution> = match opts.day {
+        1 => Box::new(y21::Day01 {}),
+        _ => unimplemented!(),
+    };
+
+    let answer: String = if opts.part == 1 {
+        solution_by_day.part_1(opts.input_file.as_str())
+    } else {
+        solution_by_day.part_2(opts.input_file.as_str())
+    };
+
+    println!("{}", answer);
 }
---
+++ src/solution.rs
@@ -0,0 +1,4 @@
+pub trait Solution {
+    fn part_1(&self, input_file: &str) -> String;
+    fn part_2(&self, input_file: &str) -> String;
+}
